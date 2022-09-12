bf0cd93d5e4c4cfd6be73048721fa1b0e65a8917
VladimirKaspy <vladimir.cvetkovic@ramsoft.com>
2022-09-12 12:21:05 +0200

    Initial setup: Created project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {
+    StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback, TouchableHighlight, Button, Alert
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const handleTextPress = () => console.log('Text pressed');
+    const handleImagePress = () => console.log('Image pressed');
+    const handleButtonPress = () => {
+        Alert.alert(
+            'Hi there',
+            'This is an alert',
+            [
+                {text: 'Yes', onPress: () => console.log('Yes')},
+                {text: 'No', onPress: () => console.log('No')}
+            ],
+            {cancelable: true});
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text numberOfLines={2} onPress={handleTextPress}>Hello Hello Morning Ball</Text>
+            <TouchableHighlight onPress={handleImagePress}>
+                <Image
+                    fadeDuration={1000}
+                    source={{
+                        width: 200,
+                        uri: 'https://picsum.photos/200/300',
+                        height: 300,
+                    }}
+                />
+            </TouchableHighlight>
+            <Button color={"orange"} title={'Click Me'} onPress={handleButtonPress}/>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
